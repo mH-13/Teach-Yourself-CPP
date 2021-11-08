bead0de9c1cc56d3842f9d53a9a58a88851928ed
Mehedi Hasan <93500871+mH-13@users.noreply.github.com>
2021-11-08 09:43:01 +0600

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+
+    cout << "Enter a number you fool! \n";
+    cin >> a;
+
+    cout << "Enter another number: \n";
+    cin >> b;
+
+    sum = a + b;
+    cout << "The sum of the two numbers is " << sum << endl;
+
+    return 0;
+}
