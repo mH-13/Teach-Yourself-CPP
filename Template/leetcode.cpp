a39f0294471180ed39d93656c34e7603ad03cfd4
mH-13 <mh13.mehedi.hasan@gmail.com>
2024-12-21 23:42:08 +0600

    Algo update

---
+++ Template/leetcode.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Example function for a problem
+class Solution {
+public:
+    int exampleFunction(vector<int>& nums) {
+        // Your logic here
+        return 0;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 2, 3, 4};
+    cout << solution.exampleFunction(nums) << endl;
+    return 0;
+}
